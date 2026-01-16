f87ec1feb48f6a95359393d8eb9294f1137fae24
Luziânia <99938142+luziania12@users.noreply.github.com>
2026-01-16 18:02:57 -0300

    Configure CORS and dotenv in app.js
    Added CORS middleware with specific options and ensured dotenv is configured correctly.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,18 @@
-require('dotenv').config(); // ✅ PRIMEIRA LINHA
+ 
 
 const express = require('express');
 const connectDB = require('./config/db');
 const booksRoutes = require('./routes/api/books');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
 // Middleware
+const corsOptions = { 
+origin: "YOUR_FRONTEND_URL", // frontend URI (ReactJS) 
+} 
+app.use(cors(corsOptions)); 
 app.use(express.json({ extended: false }));
 app.use(cors({ origin: true, credentials: true }));
 
